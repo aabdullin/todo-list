d42d96fba684806543f7e51fff6bc7e7f433dd56
Artur <rutrika@gmail.com>
2022-01-22 19:41:18 -0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import items from "./db.js";
+import { items } from "./db.js";
 
 function JobCard(props) {
   const { key, value } = props;
   console.log(props);
   return (
-    <>
-      {key} {value.name} {value.position}
-    </>
+    <div className="job">
+      <div className="one">
+        <div className="company">
+          <span>{value.name}</span>
+          {value.new && <span className="new-job">New</span>}
+          {value.featured && <span className="featured-job">Featured</span>}
+        </div>
+        <div className="position">{value.position}</div>
+        <div className="meta-info"></div>
+        {value.date} {value.type} {value.location}
+      </div>
+      <div className="tags">{value.tags}</div>
+    </div>
   );
 }
 
-function JobList(items) {
+function JobList({ items }) {
+  const [search, setSearch] = useState("");
   return (
     <div>
-      {items.map((item) => {
-        // return <JobCard key={item.key} value={item} />;
-        console.log(items);
-      })}
+      <div className="header"></div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+      <div className="job-list">
+        {items
+          .filter((item) => {
+            return item.name.startsWith(search);
+          })
+          .map((item) => {
+            return <JobCard key={item.key} value={item} />;
+          })}
+      </div>
     </div>
   );
 }
