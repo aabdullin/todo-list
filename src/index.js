3a714b398fe9ff4f7db402fd038ba8dcc68f4798
Artur <rutrika@gmail.com>
2022-06-02 09:26:23 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,156 +1,55 @@
-import React, { createContext, useContext, useReducer, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import useAxios from "axios-hooks";
+import "./index.css";
 
-// const fakeData = [
-//   {
-//     id: 1,
-//     description: "cleaning",
-//   },
-//   {
-//     id: 1,
-//     description: "laundry",
-//   },
-//   {
-//     id: 1,
-//     description: "cooking",
-//   },
-//   {
-//     id: 1,
-//     description: "coding preparation",
-//   },
-// ];
-
-const todoContext = createContext({
-  todos: [],
-  addTodo: () => {},
-  editTodo: () => {},
-});
-const TodoProvider = ({ children }) => {
-  function todoReducer(state, action) {
-    console.log(state);
-    console.log(action);
-    switch (action.type) {
-      case "add":
-        return {
-          todos: [
-            {
-              ...action.todo,
-              id: state.todos.length + 1,
-            },
-            ...state.todos,
-          ],
-        };
-      case "edit":
-        return {
-          todos: state.todos.map((u) => {
-            if (u.id === action.todo.id) {
-              return action.todo;
-            }
-
-            return u;
-          }),
-        };
-      case "reset":
-        return {
-          keys: "",
-        };
-      default:
-        throw new Error();
-    }
-  }
-  const [todosState, todosDispatch] = useReducer(todoReducer, { todos: [] });
-
-  const addTodo = (todo) => {
-    todosDispatch({ type: "add", todo });
-  };
-
-  const editTodo = (todo) => {
-    todosDispatch({ type: "edit", todo });
-  };
-
+const CryptoCard = (props) => {
+  console.log(props);
+  const { key, value } = props;
   return (
-    <todoContext.Provider
-      value={{
-        todos: [...todosState.todos],
-        addTodo,
-        editTodo,
-      }}
-    >
-      {children}
-    </todoContext.Provider>
+    <div className="crypto-card">
+      {key}
+      <h2>{value.name}</h2>
+      <img className="card-image" src={value.image} alt={value.name} />
+      <div className="crypto-card-list">
+        <li>Price: ${value.current_price} </li>
+        <li>
+          Change (24h):{" "}
+          {Math.round(value.price_change_percentage_24h * 100) / 100} %
+        </li>
+      </div>
+      {value.market_cap}
+    </div>
   );
 };
 
-const useTodos = () => {
-  const context = useContext(todoContext);
-  if (context === undefined) {
-    throw new Error("useTodos must be used within a UsersProvider");
-  }
-  return context;
-};
+const CryptoList = () => {
+  const [search, setSearch] = useState("");
+  const [{ data, loading, error }, refetch] = useAxios(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
+  );
 
-function ViewTodos() {
-  const { todos, addTodo } = useTodos();
-  const [todo, setTodo] = useState("");
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  console.log(data);
+
   return (
-    <>
+    <div className="d-flex flex-row">
       <input
         type="text"
-        name="todo"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            addTodo({
-              description: todo,
-            });
-            setTodo("");
-          }
-        }}
+        name="name"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <ul>
-        Todo List:
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.id} {todo.description}
-          </li>
-        ))}
-      </ul>
-    </>
+      {data
+        .filter((item) => {
+          return item.id.startsWith(search);
+        })
+        .map((item) => {
+          return <CryptoCard key={item.id} value={item} />;
+        })}
+    </div>
   );
-}
+};
 
-// function ToDoList({ }) {
-//   return (
-//     <ul>
-//       Todo List:
-//       {todoItems.map((todo) => (
-//         <li key={todo.id}>
-//           {todo.id} {todo.description}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// function ToDoList1({ todoItems }) {
-//   return (
-//     <ul>
-//       {todoItems.map((todo) => {
-//         console.log(todo);
-//         return (
-//           <li key={todo.id}>
-//             {todo.id} {todo.description}
-//           </li>
-//         );
-//       })}
-//     </ul>
-//   );
-// }
-
-ReactDOM.render(
-  <TodoProvider>
-    <ViewTodos />
-  </TodoProvider>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<CryptoList />, document.querySelector("#root"));
